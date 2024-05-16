a1761220afeb97023fc675e1e47645a5e5b78003
Antonio Martinez <tonhkw@Antonios-MacBook-Air.local>
2024-05-16 04:02:21 -0500

    addind decomposition analysis

--- Examples/Time Series/eda.py
+++ Examples/Time Series/eda.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Exploratory Data Analysis (EDA)
+#Performing EDA on a time series involves plotting the data, checking for trends, seasonality, and anomalies.
+
 # Load your time series data
 df = pd.read_csv('monthly_sales.csv', parse_dates=['date'], index_col='date')
 
